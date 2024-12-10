a5e06e72ba26fa130527d46625e9445cbe14e525
comradewinnie <alexrazg06@gmail.com>
2024-12-10 09:26:50 +0200

    Update pizza_app.cs

--- C#/pizza_app.cs
+++ C#/pizza_app.cs
@@ -2,19 +2,72 @@
 
 using System;
 using Microsoft.Data.Sqlite;
-public class Program
+using Microsoft.Data.SqlClient;
+
+class Program
 {
     public static void Main()
     {
 		// Connection to database string SQLite
 		string connectionString = "Data Source=pizza_store.db";
-		try {
-			// Create and open SQLite connection
+		try 
+		{
+			var pizzaCtrl = new PizzaCtrl(connectionString);
+			
+			while (true)
+			{
+				Console.WriteLine("Choose action: 'add', 'print', or 'exit'.");
+				var userCommand = Console.ReadLine();
+				
+				switch (userCommand)
+				{
+					case "add":
+						pizzaCtrl.AddPizza();
+						break;
+					case "print":
+						pizzaCtrl.PrintPizzas();
+						break;
+					case "exit":
+						return;
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("An error occurred: " + ex.Message);	
+		}
+	}
+	
+	public class PizzaCtrl
+	{
+		private readonly string connectionString;
+		
+		public PizzaCtrl(string connectionString)
+		{
+			this.connectionString = connectionString;
+			CreatePizzaTable();
+		}
+		
+		public void AddPizza()
+		{
+			Console.WriteLine("Please, enter pizza name:");
+			string pizzaName = Console.ReadLine();
+			
+			Console.WriteLine("Please, enter pizza size:");
+			string pizzaSize = Console.ReadLine();
+			
+			Console.WriteLine("Please, enter pizza price:");
+			double pizzaPrice = Convert.ToDouble(Console.ReadLine());
+			
+			AddPizzaToTable(pizzaName, pizzaSize, pizzaPrice);
+		}
+		
+		private void CreatePizzaTable()
+		{
 			using (var connection = new SqliteConnection(connectionString))
 			{
 				connection.Open();
 				
-				// Create table
 				var createTableCmd = connection.CreateCommand();
 				createTableCmd.CommandText = 
 					@"CREATE TABLE IF NOT EXISTS Pizzas (
@@ -25,42 +78,49 @@ public class Program
 						);";
 				createTableCmd.ExecuteNonQuery();
 				Console.WriteLine("Table created or exists already.");
-				
-				// Prompt user for pizza details
-				Console.WriteLine("Please, enter pizza name:");
-				string pizzaName = Console.ReadLine();
-				Console.WriteLine("Please, enter pizza size:");
-				string pizzaSize = Console.ReadLine();
-				Console.WriteLine("Please, enter pizza price:");
-				double pizzaPrice = Convert.ToDouble(Console.ReadLine());
-				
-				// Insert data in database
-				var insertCmd = connection.CreateCommand();
-				insertCmd.CommandText = "INSERT INTO Pizzas(Name, Size, Price) VALUES (@name, @size, @price)";
-				insertCmd.Parameters.AddWithValue("@name", pizzaName);
-				insertCmd.Parameters.AddWithValue("@size", pizzaSize);
-				insertCmd.Parameters.AddWithValue("@price", pizzaPrice);
-				
-				insertCmd.ExecuteNonQuery();
-				Console.WriteLine("Pizza is added to database.");
-				
-				// Query to display pizza database elements
-				var selectCmd = connection.CreateCommand();
-				selectCmd.CommandText = "SELECT * FROM Pizzas";
-				
-				using (var reader = selectCmd.ExecuteReader())
-				{
-					Console.WriteLine("Pizza List:");
-					while(reader.Read())
-					{
-						Console.WriteLine($"ID: {reader["ID"]}, name: {reader["Name"]}, size: {reader["Size"]}, price: {reader["Price"]}.");
-					}
-				}
 			}
 		}
-		catch (Exception ex)
+		
+		private void AddPizzaToTable(string name, string size, double price)
 		{
-			Console.WriteLine("An error occurred: " + ex.Message);	
+			using (var connection = new SqliteConnection(connectionString))
+			{
+				connection.Open();
+			
+				var insertCmd = connection.CreateCommand();
+				insertCmd.CommandText = "INSERT INTO Pizzas(Name, Size, Price) VALUES (@name, @size, @price)";
+				insertCmd.Parameters.AddWithValue("@name", name);
+				insertCmd.Parameters.AddWithValue("@size", size);
+				insertCmd.Parameters.AddWithValue("@price", price);
+
+				insertCmd.ExecuteNonQuery();
+				Console.WriteLine("Pizza is added to database.");
+			}
+		}
+		
+		private SqliteDataReader GetAllPizzasFromTable()
+		{
+			using (var connection = new SqliteConnection(connectionString))
+			{
+				connection.Open();
+				
+				var selectCmd = connection.CreateCommand();
+				selectCmd.CommandText = "SELECT * FROM Pizzas";
+
+				return selectCmd.ExecuteReader();
+			}
+		}
+		
+		public void PrintPizzas()
+		{
+			using (var reader = GetAllPizzasFromTable())
+			{				
+				Console.WriteLine("Pizza List:");
+				while(reader.Read())
+				{
+					Console.WriteLine($"ID: {reader["ID"]}, name: {reader["Name"]}, size: {reader["Size"]}, price: {reader["Price"]}.");
+				}
+			}
 		}
 	}
 }
