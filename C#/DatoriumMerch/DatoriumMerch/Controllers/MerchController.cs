30757a7156400fcf7fcebc3e26ef019650b6642d
comradewinnie <alexrazg06@gmail.com>
2025-02-18 10:19:29 +0200

    Add files via upload

---
+++ C#/DatoriumMerch/DatoriumMerch/Controllers/MerchController.cs
@@ -0,0 +1,38 @@
+﻿using DatoriumMerch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatoriumMerch.Controllers
+{
+    [Route("api/datorium-merch")]
+    [ApiController]
+    public class MerchController : ControllerBase
+    {
+        private readonly MerchContext _dbContext;
+
+        public MerchController(MerchContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet("ok")]
+        public ActionResult<string> GetOk()
+        {
+            return "OK";
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Merch>> GetMerch()
+        {
+            return _dbContext.Merches.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult PostMerch(Merch merch)
+        {
+            _dbContext.Merches.Add(merch);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
