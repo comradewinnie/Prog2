e10395c83f604be63cf8dd99310707a6c527b135
comradewinnie <alexrazg06@gmail.com>
2025-02-18 10:20:19 +0200

    Add files via upload

---
+++ C#/HangmanGameV2/HangmanGame/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace HangmanGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            labelWelcome = new Label();
+            labelWelcome2 = new Label();
+            textBoxLogin = new TextBox();
+            textBoxPassword = new TextBox();
+            labelLogin = new Label();
+            labelPassword = new Label();
+            buttonLoginRegister = new Button();
+            SuspendLayout();
+            // 
+            // labelWelcome
+            // 
+            labelWelcome.AutoSize = true;
+            labelWelcome.BackColor = Color.Transparent;
+            labelWelcome.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Regular, GraphicsUnit.Point);
+            labelWelcome.ForeColor = SystemColors.Control;
+            labelWelcome.Location = new Point(110, 9);
+            labelWelcome.Name = "labelWelcome";
+            labelWelcome.Size = new Size(261, 47);
+            labelWelcome.TabIndex = 0;
+            labelWelcome.Text = "Welcome to\r";
+            labelWelcome.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelWelcome2
+            // 
+            labelWelcome2.AutoSize = true;
+            labelWelcome2.BackColor = Color.Transparent;
+            labelWelcome2.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Regular, GraphicsUnit.Point);
+            labelWelcome2.ForeColor = Color.DarkRed;
+            labelWelcome2.Location = new Point(30, 56);
+            labelWelcome2.Name = "labelWelcome2";
+            labelWelcome2.Size = new Size(409, 47);
+            labelWelcome2.TabIndex = 1;
+            labelWelcome2.Text = "the Hangman game!";
+            labelWelcome2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBoxLogin
+            // 
+            textBoxLogin.BackColor = Color.DarkRed;
+            textBoxLogin.Font = new Font("Britannic Bold", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxLogin.ForeColor = SystemColors.Control;
+            textBoxLogin.Location = new Point(212, 139);
+            textBoxLogin.Margin = new Padding(3, 2, 3, 2);
+            textBoxLogin.Name = "textBoxLogin";
+            textBoxLogin.Size = new Size(246, 34);
+            textBoxLogin.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.BackColor = Color.DarkRed;
+            textBoxPassword.Font = new Font("Britannic Bold", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxPassword.ForeColor = SystemColors.Control;
+            textBoxPassword.Location = new Point(212, 207);
+            textBoxPassword.Margin = new Padding(3, 2, 3, 2);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(246, 34);
+            textBoxPassword.TabIndex = 3;
+            textBoxPassword.TextChanged += textBox2_TextChanged;
+            // 
+            // labelLogin
+            // 
+            labelLogin.AutoSize = true;
+            labelLogin.BackColor = Color.Transparent;
+            labelLogin.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelLogin.ForeColor = SystemColors.Control;
+            labelLogin.Location = new Point(212, 112);
+            labelLogin.Name = "labelLogin";
+            labelLogin.Size = new Size(159, 20);
+            labelLogin.TabIndex = 5;
+            labelLogin.Text = "Enter your login";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.BackColor = Color.Transparent;
+            labelPassword.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPassword.ForeColor = SystemColors.Control;
+            labelPassword.Location = new Point(212, 181);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(153, 20);
+            labelPassword.TabIndex = 6;
+            labelPassword.Text = "Enter password";
+            // 
+            // buttonLoginRegister
+            // 
+            buttonLoginRegister.BackColor = SystemColors.Control;
+            buttonLoginRegister.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonLoginRegister.ForeColor = SystemColors.ControlText;
+            buttonLoginRegister.Location = new Point(212, 261);
+            buttonLoginRegister.Margin = new Padding(3, 2, 3, 2);
+            buttonLoginRegister.Name = "buttonLoginRegister";
+            buttonLoginRegister.Size = new Size(245, 38);
+            buttonLoginRegister.TabIndex = 7;
+            buttonLoginRegister.Text = "LOGIN / REGISTER";
+            buttonLoginRegister.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(478, 320);
+            Controls.Add(buttonLoginRegister);
+            Controls.Add(labelPassword);
+            Controls.Add(labelLogin);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxLogin);
+            Controls.Add(labelWelcome2);
+            Controls.Add(labelWelcome);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "The Hangman Game";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelWelcome;
+        private Label labelWelcome2;
+        private TextBox textBoxLogin;
+        private TextBox textBoxPassword;
+        private Label labelLogin;
+        private Label labelPassword;
+        private Button buttonLoginRegister;
+    }
+}
---
+++ C#/HangmanGameV2/HangmanGame/Form2.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace HangmanGame
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonNewWord = new Button();
+            buttonNewTopic = new Button();
+            buttonLogOut = new Button();
+            buttonEditLB = new Button();
+            labelLogged = new Label();
+            tabControl1 = new TabControl();
+            tabPageWords = new TabPage();
+            tabPageTopics = new TabPage();
+            tabPageLB = new TabPage();
+            labelTotal = new Label();
+            labelTotalText = new Label();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonNewWord
+            // 
+            buttonNewWord.Location = new Point(14, 254);
+            buttonNewWord.Margin = new Padding(3, 2, 3, 2);
+            buttonNewWord.Name = "buttonNewWord";
+            buttonNewWord.Size = new Size(224, 22);
+            buttonNewWord.TabIndex = 0;
+            buttonNewWord.Text = "Add new word";
+            buttonNewWord.UseVisualStyleBackColor = true;
+            // 
+            // buttonNewTopic
+            // 
+            buttonNewTopic.Location = new Point(244, 254);
+            buttonNewTopic.Margin = new Padding(3, 2, 3, 2);
+            buttonNewTopic.Name = "buttonNewTopic";
+            buttonNewTopic.Size = new Size(207, 22);
+            buttonNewTopic.TabIndex = 1;
+            buttonNewTopic.Text = "Add new topic";
+            buttonNewTopic.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogOut
+            // 
+            buttonLogOut.Location = new Point(369, 7);
+            buttonLogOut.Margin = new Padding(3, 2, 3, 2);
+            buttonLogOut.Name = "buttonLogOut";
+            buttonLogOut.Size = new Size(83, 22);
+            buttonLogOut.TabIndex = 4;
+            buttonLogOut.Text = "Log out";
+            buttonLogOut.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditLB
+            // 
+            buttonEditLB.Location = new Point(14, 287);
+            buttonEditLB.Margin = new Padding(3, 2, 3, 2);
+            buttonEditLB.Name = "buttonEditLB";
+            buttonEditLB.Size = new Size(437, 22);
+            buttonEditLB.TabIndex = 5;
+            buttonEditLB.Text = "Edit leaderboard";
+            buttonEditLB.UseVisualStyleBackColor = true;
+            // 
+            // labelLogged
+            // 
+            labelLogged.AutoSize = true;
+            labelLogged.Location = new Point(10, 7);
+            labelLogged.Name = "labelLogged";
+            labelLogged.Size = new Size(97, 15);
+            labelLogged.TabIndex = 6;
+            labelLogged.Text = "You logged in as:";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageWords);
+            tabControl1.Controls.Add(tabPageTopics);
+            tabControl1.Controls.Add(tabPageLB);
+            tabControl1.Location = new Point(10, 32);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(445, 209);
+            tabControl1.TabIndex = 7;
+            // 
+            // tabPageWords
+            // 
+            tabPageWords.Location = new Point(4, 24);
+            tabPageWords.Margin = new Padding(3, 2, 3, 2);
+            tabPageWords.Name = "tabPageWords";
+            tabPageWords.Padding = new Padding(3, 2, 3, 2);
+            tabPageWords.Size = new Size(437, 181);
+            tabPageWords.TabIndex = 0;
+            tabPageWords.Text = "Words";
+            tabPageWords.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTopics
+            // 
+            tabPageTopics.Location = new Point(4, 24);
+            tabPageTopics.Margin = new Padding(3, 2, 3, 2);
+            tabPageTopics.Name = "tabPageTopics";
+            tabPageTopics.Padding = new Padding(3, 2, 3, 2);
+            tabPageTopics.Size = new Size(437, 181);
+            tabPageTopics.TabIndex = 1;
+            tabPageTopics.Text = "Topics";
+            tabPageTopics.UseVisualStyleBackColor = true;
+            // 
+            // tabPageLB
+            // 
+            tabPageLB.Location = new Point(4, 24);
+            tabPageLB.Margin = new Padding(3, 2, 3, 2);
+            tabPageLB.Name = "tabPageLB";
+            tabPageLB.Padding = new Padding(3, 2, 3, 2);
+            tabPageLB.Size = new Size(437, 181);
+            tabPageLB.TabIndex = 2;
+            tabPageLB.Text = "Leaderboard";
+            tabPageLB.UseVisualStyleBackColor = true;
+            // 
+            // labelTotal
+            // 
+            labelTotal.AutoSize = true;
+            labelTotal.Location = new Point(17, 322);
+            labelTotal.Name = "labelTotal";
+            labelTotal.Size = new Size(35, 15);
+            labelTotal.TabIndex = 8;
+            labelTotal.Text = "Total:";
+            // 
+            // labelTotalText
+            // 
+            labelTotalText.AutoSize = true;
+            labelTotalText.Location = new Point(59, 322);
+            labelTotalText.Name = "labelTotalText";
+            labelTotalText.Size = new Size(152, 15);
+            labelTotalText.TabIndex = 9;
+            labelTotalText.Text = "n words    n topics    n users";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 346);
+            Controls.Add(labelTotalText);
+            Controls.Add(labelTotal);
+            Controls.Add(tabControl1);
+            Controls.Add(labelLogged);
+            Controls.Add(buttonEditLB);
+            Controls.Add(buttonLogOut);
+            Controls.Add(buttonNewTopic);
+            Controls.Add(buttonNewWord);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form2";
+            Text = "The Hangman Game [ADMIN]";
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonNewWord;
+        private Button buttonNewTopic;
+        private Button buttonLogOut;
+        private Button buttonEditLB;
+        private Label labelLogged;
+        private TabControl tabControl1;
+        private TabPage tabPageWords;
+        private TabPage tabPageTopics;
+        private TabPage tabPageLB;
+        private Label labelTotal;
+        private Label labelTotalText;
+    }
+}
