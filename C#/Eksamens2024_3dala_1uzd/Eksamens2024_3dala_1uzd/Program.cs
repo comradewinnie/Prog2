89514467535064943206b6df46524fadc93c2ac3
comradewinnie <alexrazg06@gmail.com>
2025-04-22 09:25:31 +0300

    Add files via upload

---
+++ C#/Eksamens2024_3dala_1uzd/Eksamens2024_3dala_1uzd/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Doktorats doktorats = new Doktorats();
+        doktorats.IevaditVertibas();
+        doktorats.IzdrukatVertibas();
+    }
+}
+class Doktorats
+{
+    public string DoktorataNosaukums;
+    public int DoktorataPacientuSkaits;
+
+    public void IevaditVertibas()
+    {
+        Console.WriteLine("Ievadiet doktorāta nosaukumu: ");
+        DoktorataNosaukums = Console.ReadLine();
+
+        Console.WriteLine("Ievadiet pacientu skaitu: ");
+        DoktorataPacientuSkaits = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void IzdrukatVertibas()
+    {
+        Console.WriteLine($"Doktorāts {DoktorataNosaukums} apkalpo {DoktorataPacientuSkaits} pacientus.");
+    }
+}
