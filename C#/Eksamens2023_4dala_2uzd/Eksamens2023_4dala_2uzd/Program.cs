3ef2814f7899f52da6452404ce52e4d1fea1bb11
comradewinnie <alexrazg06@gmail.com>
2025-04-29 19:36:52 +0300

    Add files via upload

--- C#/Eksamens2023_4dala_2uzd/Eksamens2023_4dala_2uzd/Program.cs
+++ C#/Eksamens2023_4dala_2uzd/Eksamens2023_4dala_2uzd/Program.cs
@@ -2,34 +2,37 @@
 {
     static void Main(string[] args)
     {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         string alphabet = "_AĀBCČDEĒFGĢHIĪJKĶLĻMNŅOPRSŠTUŪVZŽ";
         string[] words = new string[alphabet.Length];
 
-        while (words.Any(x => x == null)) // пока какая-то позиция в words пустая, цикл работает
+        while (words.Any(i => i == null))
         {
-            Console.WriteLine("Ievadiet vārdu:");
+            Console.Write("Ievadiet vārdu: ");
             string word = Console.ReadLine();
 
             while (!IsWordValid(word))
             {
                 Console.WriteLine("Ievadītais vārds nav derīgs!");
-                Console.WriteLine("Ievadiet vārdu:");
+                Console.Write("Ievadiet vārdu: ");
                 word = Console.ReadLine();
             }
 
             char firstLetter = word[0];
-            int letterIndex = alphabet.IndexOf(firstLetter);  // проверяет, на какой позиции в массиве алфавита находится буква, которая является первой буквой для введённого слова
+            int letterIndex = alphabet.IndexOf(firstLetter);
 
             words[letterIndex] = word;
             Console.WriteLine($"Pievienoju vārdu {letterIndex}. vietā");
-        } 
+        }
     }
 
     static bool IsWordValid(string word)
     {
         return word.Length > 1 &&
-            Char.IsUpper(word[0]) &&   // заглавная ли первая буква
-            word.Skip(1).All(char.IsLower) &&   // строчные ли все буквы, кроме первой
-            word.All(char.IsLetter);    // все ли символы - буквы
+            Char.IsUpper(word[0]) &&
+            word.Skip(1).All(char.IsLower) &&
+            word.All(char.IsLetter);
     }
 }
